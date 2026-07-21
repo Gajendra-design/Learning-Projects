aa4da2fa740544089668d78b1afadcf8d7ebda7a
Gajendra Sharma <sharmagajendra167@gmail.com>
2026-07-21 09:49:45 +0530

    in shop page applied fakestore api and sucessfully setup local storage and useState of products

--- React/sky-mart/src/Context/MyStore.jsx
+++ React/sky-mart/src/Context/MyStore.jsx
@@ -6,7 +6,13 @@ export const MyStoreProvider = ({ children }) => {
 
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [isProfileOpen,setIsProfileOpen] = useState(false);
-  console.log(isCartOpen);
+  const [products,setProducts] = useState(JSON.parse(localStorage.getItem('products')) || [])
+  
+  console.log('is cart open',isCartOpen);
+  console.log('is profile open',isProfileOpen);
+  console.log('products in ls',JSON.parse(localStorage.getItem('products')));
+  
+  
   
 
   return (
@@ -15,7 +21,9 @@ export const MyStoreProvider = ({ children }) => {
         isCartOpen,
         setIsCartOpen,
         isProfileOpen,
-        setIsProfileOpen
+        setIsProfileOpen,
+        products,
+        setProducts
       }}
     >
       {children}
--- React/sky-mart/src/components/Pages/Shop.jsx
+++ React/sky-mart/src/components/Pages/Shop.jsx
@@ -1,88 +1,28 @@
-import React from 'react';
-import { Search, ChevronDown} from 'lucide-react';
+import React, { useContext, useEffect } from 'react';
+import { Search, ChevronDown } from 'lucide-react';
 import { Link } from 'react-router';
 import ProductCard from '../Particals/ProductCard';
+import axios from 'axios';
+import { MyStore } from '../../Context/MyStore';
 
 export default function Shop() {
-  // Static Dummy Products Data
-  const products = [
-    {
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-      rating: { rate: 3.9, count: 120 }
-    },
-    {
-      id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts",
-      price: 22.3,
-      description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
-      rating: { rate: 4.1, count: 259 }
-    },
-    {
-      id: 3,
-      title: "Mens Cotton Jacket",
-      price: 55.99,
-      description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
-      rating: { rate: 4.7, count: 500 }
-    },
-    {
-      id: 4,
-      title: "Mens Casual Slim Fit",
-      price: 15.99,
-      description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png",
-      rating: { rate: 2.1, count: 430 }
-    },
-    {
-      id: 5,
-      title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-      price: 695,
-      description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl.",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png",
-      rating: { rate: 4.6, count: 400 }
-    },
-    {
-      id: 6,
-      title: "Solid Gold Petite Micropave",
-      price: 168,
-      description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States.",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-      rating: { rate: 3.9, count: 70 }
-    },
-    {
-      id: 7,
-      title: "White Gold Plated Princess",
-      price: 9.99,
-      description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement.",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
-      rating: { rate: 3, count: 400 }
-    },
-    {
-      id: 8,
-      title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
-      price: 10.99,
-      description: "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
-      rating: { rate: 1.9, count: 100 }
-    }
-  ];
+
+  const { products, setProducts } = useContext(MyStore)
+
+  const getProducts = async (url) => {
+    const response = await axios.get(url);
+    const products = response.data
+    localStorage.setItem('products', JSON.stringify(products))
+    setProducts(products)
+  }
+
+  //i am using useEffect for handeling infinite calling of getProduct funciton because of setProduct rerender and the call as we studied
+  useEffect(() => { getProducts('https://fakestoreapi.com/products') }, [])
+
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 space-y-6 font-sans text-slate-100">
-      
+
       {/* ================= SHOP HEADER ================= */}
       <div className="space-y-1">
         <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-white">
@@ -96,7 +36,7 @@ export default function Shop() {
       {/* ================= SEARCH & FILTER BAR ================= */}
       <div className="bg-slate-900 border border-slate-800 rounded-2xl p-2.5 sm:p-3 shadow-md">
         <div className="flex flex-col sm:flex-row items-center gap-3">
-          
+
           {/* Search Input UI */}
           <div className="relative flex-1 w-full">
             <Search className="absolute left-3.5 top-1/2 -translate-y-1/2 w-4 h-4 text-slate-400" />
@@ -143,7 +83,7 @@ export default function Shop() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center pt-2">
         {products.map((item) => (
           <Link to='/product-display/7'>
-          <ProductCard key={item.id} product={item} />
+            <ProductCard key={item.id} product={item} />
           </Link>
         ))}
       </div>
